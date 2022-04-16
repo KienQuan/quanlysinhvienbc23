92eaeae2461d9ce1114d7f7d0f8b694770a1b42e
KienQuan <102026057+KienQuan@users.noreply.github.com>
2022-04-16 18:24:15 +0700

    abc

--- models/SinhVien.js
+++ models/SinhVien.js
@@ -8,12 +8,9 @@ function SinhVien() {
     this.diemToan = '';
     this.diemLy = '';
     this.diemHoa = ''; //conflict code: xung dot code 
-<<<<<<< HEAD
     this.xepLoai = function(){
         console.log('dev b code ham xep loai')
-=======
     this.tinhDiemTrungBinh = function(){
         console.log('Tinh Diem Trung Binh!');
->>>>>>> 0d41a5449dbfed9ed60fd53a3da632596eeff8d3
     }
 }
